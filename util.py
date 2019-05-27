a4b24aabffe224a73f18dd2fc54672aad5227b0f
Yuxing Chen <yxchen2828@gmail.com>
2019-05-27 16:03:01 -0700

    clean tweets (no emoji), other special cases

--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from collections import Counter
 from scipy.io import loadmat
+import re
 
 def load_mat_data(path ="./data/data.mat"):
     return loadmat(path)
@@ -14,3 +15,23 @@ def build_vocab(docs, least_freq=3):
         vocab.update(Counter(d.split(" ")))
     vocab = {k for k, v in vocab.items() if v >= least_freq}
     return list(vocab)
+
+
+def load_textfile(path = "./data/USA_tokenized.txt"):
+    tweets_lst = []
+    with open(path) as f:
+        for line in f:
+            values = line.split("\t")
+            tweets_lst.append(values[-1].rstrip('\n'))
+    return tweets_lst
+
+
+def clean_tweets(text):
+    text = re.sub(r'&[a-z]{3};', ' <sym> ', text)  # '&amp;'
+    text = re.sub(r'&gt;', '>', text)
+    text = re.sub(r'&lt;', '<', text) 
+    text = re.sub(r'@\w+', ' <user> ', text)
+    ENCODE_EMOJI = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)  # emoji
+    text = ENCODE_EMOJI.sub('', text)
+    text= re.sub(' +', ' ', text)
+    return text.strip().lower()
